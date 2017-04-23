3155cf2c2cac9a6bfbeaba75ae50c4cfe0deac3b
David <dmayola@gmail.com>
2017-04-23 17:36:39 +0200

    * WindowObserver

--- CinderProject/include/InputController.h
+++ CinderProject/include/InputController.h
@@ -17,13 +17,25 @@ private:
 	virtual void onRightMouse(CoordsInt mousePosition) = 0;
 };
 
-class InputController
+class WindowObserver
+{
+public:
+	virtual void onWorldDimensionsChange( unsigned total_pixel_width_, unsigned total_pixel_height_ ) = 0;
+	virtual void onLevelGridChanged( unsigned width_, unsigned height_ ) = 0;
+	virtual void onWindowSizeChange( unsigned width_, unsigned height_ ) = 0;
+};
+
+class InputController : public WindowObserver
 {
 public:
 	InputController( cinder::app::WindowRef window_ );
 
 	void RegisterEventListener( EventListener* listener );
 	void UnregisterEventListener( EventListener* listener );
+
+	void onWorldDimensionsChange( unsigned total_pixel_width_, unsigned total_pixel_height_ ) override;
+	void onLevelGridChanged( unsigned width_, unsigned height_ ) override;
+	void onWindowSizeChange( unsigned width_in_pixels_, unsigned height_in_pixels_ ) override;
 
 private:
 
@@ -35,4 +47,14 @@ private:
 	cinder::signals::ScopedConnection m_onMouseClickConnection;
 
 	std::vector<EventListener*> listeners;
+
+	unsigned totalWidthInPixels = 0;
+	unsigned totalHeightInPixels = 0;
+
+	unsigned worldWidthInPixels = 0;
+	unsigned worldHeightInPixels = 0;
+
+	unsigned gardenWidthDimension = 0;
+	unsigned gardenHeightDimension = 0;
+
 };
